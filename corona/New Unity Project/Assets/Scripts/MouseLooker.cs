0b940217d52024b320f8747f63bf5aaec112fd7e
Yugraj124 <54675010+Yugraj124@users.noreply.github.com>
2020-09-19 23:09:39 +0530

    added movement mouse look and camera follow

---
+++ corona/New Unity Project/Assets/Scripts/MouseLooker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLooker : MonoBehaviour
+{
+    float x, y;
+    Vector2 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.mousePosition.x;
+        y = Input.mousePosition.y;
+
+        pos = Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2((y-pos.y), (x-pos.x)) * Mathf.Rad2Deg;
+
+        transform.eulerAngles =new Vector3(0,0,angle);
+    }
+}
---
+++ corona/New Unity Project/Assets/Scripts/mover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour
+{
+    public float speed = 10f;
+    float x, y;
+    Vector2 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+        pos = transform.position;
+
+
+        if (x != 0 && y != 0)
+        {
+            pos.x += (x * speed * Time.deltaTime);
+            pos.y += (y * speed * Time.deltaTime);
+        }
+        else
+        {
+
+
+            if (x != 0)
+                pos.x += (x * 1.414f * speed * Time.deltaTime);
+            if (y != 0)
+                pos.y += (y * 1.414f * speed * Time.deltaTime);
+        }
+
+
+        transform.position = pos;
+    }
+}
