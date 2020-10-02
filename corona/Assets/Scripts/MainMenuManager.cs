98a1efd577ded20e092e2a70f7016da058af0f7b
Nipun Topno <nipuntopno77@gmail.com>
2020-10-02 21:57:49 +0530

    added main menu and pause menu

---
+++ corona/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+	public Button QuitButton;
+
+    private void Awake()
+    {
+		displayExitButton();
+    }
+
+	void displayExitButton()
+	{
+		switch (Application.platform)
+		{
+			case RuntimePlatform.WindowsPlayer:
+			case RuntimePlatform.OSXPlayer:
+			case RuntimePlatform.LinuxPlayer:
+				QuitButton.interactable = true;
+				break;
+			default:
+				QuitButton.interactable = false;
+				break;
+		}
+	}
+
+	public void loadScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ corona/Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenu;
+    public GameObject player;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        player.GetComponent<MouseLooker>().enabled = false;
+        GamePaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        player.GetComponent<MouseLooker>().enabled = true;
+        GamePaused = false;
+    }
+
+    public void loadScene(string sceneToLoad)
+    {
+        Resume();
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
